514e53bde2ad47c1216c9a86ec62aea0dadf5d3c
bing876 <2296308012@qq.com>
2026-09-02 19:44:36 +0800

    add routing test foundation

---
+++ src/tests/routing.test.js
@@ -0,0 +1,10 @@
+export function routingTest(){
+  return {
+    name:'routing validation',
+    checks:[
+      'primary model selection',
+      'assistant model selection',
+      'cost aware routing'
+    ]
+  };
+}
